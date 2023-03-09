ccae60d1efe4561b101475df03709ff2ad9b7297
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-03-09 12:28:17 +0100

    Project is working with a great structure

--- data_acquisition/to_csv.py
+++ data_acquisition/to_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
-def create_a_CSV(entreprise_list,my_header_list,csv_name) :
+def create_pandasDF_financeYahoo(entreprise_list,my_header_list) :
     for elem in entreprise_list :
         elem.pop()  # remove the last element of each inner list
     df = pd.DataFrame(data=entreprise_list, columns=my_header_list)
-    df.to_csv(csv_name, index=False)
+    return df
 
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from data_acquisition.scrapping import main_scrap_financeYahoo
-from data_acquisition.to_csv import create_a_CSV
+from data_acquisition.to_csv import create_pandasDF_financeYahoo
+import pandas as pd
+def main() :
+    #Scrapping
+    entreprise_list,my_header_list = main_scrap_financeYahoo("https://finance.yahoo.com/most-active?offset=0&count=100")
+    #create data frame
+    df1 = create_pandasDF_financeYahoo(entreprise_list,my_header_list)
 
-entreprise_list,my_header_list = main_scrap_financeYahoo("https://finance.yahoo.com/most-active?offset=0&count=100")
-create_a_CSV(entreprise_list,my_header_list,"data/financeYahoo_dataframe.csv")
+    '''Since we know that financeYahoo only display 200 entreprise,
+    we can hardcode the url to scrap on the two differents page (100 entreprise / page).
+    '''
+    entreprise_list,my_header_list = main_scrap_financeYahoo("https://finance.yahoo.com/most-active?offset=100&count=100")
+    #create data frame
+    df2 = create_pandasDF_financeYahoo(entreprise_list,my_header_list)
+    #concat the 2 DataFrame
+    df_concat = pd.concat([df1, df2], axis=0, ignore_index=True)
+    #export it as a CV
+    df_concat.to_csv("data/financeYahoo_dataframe.csv", index=False)
+
+if __name__ == "__main__" :
+    main()
