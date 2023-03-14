3f254a9b5e97b53ae70869738fd8450e27a44d0b
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-03-14 12:29:15 +0100

    New Dag and scrapping (still not working with Airflows)

--- dag_file_airflow.py
+++ dag_file_airflow.py
@@ -42,5 +42,24 @@ scrap_financeYahoo_operator = PythonOperator(
     dag=dag
 )
 
-
 scrap_financeYahoo_operator
+
+# -------------------------
+
+'''from data_acquisition.thread_scrapping import launch_threading
+import pandas as pd
+import os
+
+def main() :
+    filename = "data/financeYahoo_dataframe.csv"
+    if os.path.exists(filename):
+        os.remove(filename)
+    df1,df2 = launch_threading()
+    #concat the 2 DataFrame
+    df_concat = pd.concat([df1, df2], axis=0, ignore_index=True)
+    #export it as a CV
+    df_concat.to_csv(filename, index=False)
+
+if __name__ == "__main__" :
+    main()'''
+
--- data_acquisition/scrapping.py
+++ data_acquisition/scrapping.py
@@ -1,16 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import Bys
 
 
 def get_the_page_firefox(link) :
-        options = webdriver.FirefoxOptions()
+        '''options = webdriver.FirefoxOptions()
         options.accept_insecure_certs = True
         driver = webdriver.Remote(
         command_executor='http://localhost:4444/wd/hub',
         options=options
-        )
-        
-        #driver = webdriver.Chrome()
+        )'''
+        driver = webdriver.Firefox()
         driver.set_page_load_timeout(60)
         driver.get(link)
         return driver
